f27660411382534dc3714b2c55688d289b8e70eb
= <laurenduartefagundes@gmail.com>
2025-03-30 23:22:37 -0300

    Att

--- mulheres.js
+++ mulheres.js
@@ -1,33 +1,57 @@
 const express = require("express")
 const router = express.Router()
+const { v4: uuidv4 } = require('uuid');
 
 const app = express()
 const porta = 3333
+
+//LISTA MULHERES
 const mulheres = [
     {
+        id: '1',
         nome: 'Lauren Duarte', 
         imagem: 'https://bit.ly/3FKpFaz',
         minibio: 'Estudante de Análise e Desenvolvimento de Sistemas'
     },
     {
+        id: '2',
         nome: 'Simara Conceição',
         imagem: 'https://bit.ly/3LJIyOF',
         minibio: 'Desenvolvedora e Instrutora'
     },
     {
+        id: '3',
         nome: 'Iana Chan',
         imagem: 'https://bit.ly/3JCXBqP',
         minibio: 'Fundadora da Programaria'
     }
 ]
 
+//GET
 function mostraMulheres(request, response) {
     response.json(mulheres)
 }
 
+//POST
+function criaMulher(request, response){
+    const novaMulher = {
+        id: uuidv4(),
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio
+    }
+
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+
+
+//PORTA
 function mostraPorta() {
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
 app.use(router.get('/mulheres', mostraMulheres))
+app.use(router.post('/mulheres', criaMulher))
 app.listen(porta, mostraPorta)
